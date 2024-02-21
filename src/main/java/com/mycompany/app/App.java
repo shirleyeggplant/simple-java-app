598b87b6cda064a4de74963fd7f8d9130fcf6fb4
Shirley Eggplant <shirleyeggplant@gmail.com>
2024-02-21 14:12:39 +0800

    Correct Test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin~~~No problem");
+        System.out.println("Jenkin~~~");
     }
 
     public String getMessage() {
