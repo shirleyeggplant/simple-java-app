a22bf2fd61aaadd170a0ef40f2e097cc9ad3bc66
Shirley Eggplant <shirleyeggplant@gmail.com>
2024-02-21 14:20:08 +0800

    Trigger webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Jenkin~~~");
+        System.out.println("Jenkin~~~Just some changes");
     }
 
     public String getMessage() {
